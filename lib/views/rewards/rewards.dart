759a1a4c7589bf266dd97772f2093df98604ea46
MaximeArn <maxime-arnould@outlook.fr>
2021-06-15 11:48:33 +0200

    map on rewards data to generate a RewardCard per Reward

--- lib/views/rewards/rewards.dart
+++ lib/views/rewards/rewards.dart
@@ -1,10 +1,29 @@
+import 'package:cooking/data/rewards.dart';
+import 'package:cooking/models/Reward.type.dart';
+import 'package:cooking/views/rewards/widgets/rewardCard.dart';
 import 'package:flutter/material.dart';
 
-class Rewards extends StatelessWidget {
+class Rewards extends StatefulWidget {
+  @override
+  _RewardsState createState() => _RewardsState();
+}
+
+class _RewardsState extends State<Rewards> {
+  late List<Reward> rewards;
+
+  @override
+  void initState() {
+    rewards = rewardsList;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("rewards"),
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) {
+        return RewardCard(reward: rewards[index]);
+      },
+      itemCount: rewards.length,
     );
   }
 }
--- lib/views/rewards/widgets/rewardCard.dart
+++ lib/views/rewards/widgets/rewardCard.dart
@@ -1,10 +1,17 @@
+import 'package:cooking/models/Reward.type.dart';
 import 'package:flutter/material.dart';
 
-class Reward extends StatelessWidget {
-  const Reward({Key? key}) : super(key: key);
+class RewardCard extends StatelessWidget {
+  final Reward reward;
+
+  RewardCard({required this.reward});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(
+      child: Container(
+        height: 250,
+      ),
+    );
   }
 }
