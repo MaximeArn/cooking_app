d683e838cdf368a937df2e0dfeb64f82d3f4c6f6
MaximeArn <maxime-arnould@outlook.fr>
2021-06-29 22:33:11 +0200

    add Users to seed and link user to posts

--- backend/models/user.ts
+++ backend/models/user.ts
@@ -1,11 +1,16 @@
 import { Schema, model } from "mongoose";
 import { postSchema } from "./post";
 
+const PORT = process.env.PORT;
+
 export const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  avatar: { type: String, default: "" },
+  avatar: {
+    type: String,
+    default: `http://localhost:${PORT}/assets/images/avatar/default_avatar.jpg`,
+  },
   posts: { type: [postSchema], default: [] },
 });
 
--- backend/src/seed.ts
+++ backend/src/seed.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import { config } from "dotenv";
 import Post from "../models/post";
 import Reward from "../models/reward";
+import User from "../models/user";
 
 config();
 const PORT = process.env.PORT;
@@ -19,23 +20,77 @@ mongoose
   )
   .then(() => {
     Promise.all([
+      new User({
+        name: "Djibrilhn",
+        email: "djibril@gmail.com",
+        password: "Djibril",
+        avatar: `http://localhost:${PORT}/assets/images/avatar/profile_photo_1.jpg`,
+      }).save(),
+      new User({
+        name: "Gabriel",
+        email: "gabriel@gmail.com",
+        password: "Gabriel",
+        avatar: `http://localhost:${PORT}/assets/images/avatar/profile_photo_5.jpg`,
+      }).save(),
+      new User({
+        name: "Maxime",
+        email: "maxime@gmail.com",
+        password: "Maxime",
+        avatar: `http://localhost:${PORT}/assets/images/avatar/profile_photo_4.jpg`,
+      }).save(),
+      new User({
+        name: "Chloé",
+        email: "chloé@gmail.com",
+        password: "Chloé",
+        avatar: `http://localhost:${PORT}/assets/images/avatar/profile_photo_2.jpg`,
+      }).save(),
+      new User({
+        name: "Jade",
+        email: "jade@gmail.com",
+        password: "Jade",
+        avatar: `http://localhost:${PORT}/assets/images/avatar/profile_photo_3.jpg`,
+      }).save(),
+      new User({
+        name: "Anne",
+        email: "anne@gmail.com",
+        password: "Anne",
+        avatar: `http://localhost:${PORT}/assets/images/avatar/profile_photo_6.jpg`,
+      }).save(),
       new Post({
         image: `http://localhost:${PORT}/assets/images/posts/dish_1.jpg`,
+        author: "60db8137d1903a2ea01edf50",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fermentum pharetra mauris vitae bibendum. Duis gravida tellus mauris, in fermentum massa pharetra non. Aliquam rhoncus faucibus mauris, a auctor nulla feugiat nec. Quisque sed pharetra lorem. Quisque egestas, odio a cursus cursus, lectus ipsum tristique odio, sit amet condimentum tortor.",
       }).save(),
       new Post({
         image: `http://localhost:${PORT}/assets/images/posts/dish_2.jpg`,
+        author: "60db8137d1903a2ea01edf51",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fermentum pharetra mauris vitae bibendum. Duis gravida tellus mauris, in fermentum massa pharetra non. Aliquam rhoncus faucibus mauris, a auctor nulla feugiat nec. Quisque sed pharetra lorem. Quisque egestas, odio a cursus cursus, lectus ipsum tristique odio, sit amet condimentum tortor. ",
       }).save(),
       new Post({
         image: `http://localhost:${PORT}/assets/images/posts/dish_3.jpg`,
+        author: "60db8137d1903a2ea01edf52",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fermentum pharetra mauris vitae bibendum. Duis gravida tellus mauris, in fermentum massa pharetra non. Aliquam rhoncus faucibus mauris, a auctor nulla feugiat nec. Quisque sed pharetra lorem. Quisque egestas, odio a cursus cursus, lectus ipsum tristique odio, sit amet condimentum tortor. ",
       }).save(),
       new Post({
         image: `http://localhost:${PORT}/assets/images/posts/dish_4.jpg`,
+        author: "60db8137d1903a2ea01edf53",
+        description:
+          " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fermentum pharetra mauris vitae bibendum. Duis gravida tellus mauris, in fermentum massa pharetra non. Aliquam rhoncus faucibus mauris, a auctor nulla feugiat nec. Quisque sed pharetra lorem. Quisque egestas, odio a cursus cursus, lectus ipsum tristique odio, sit amet condimentum tortor.",
       }).save(),
       new Post({
         image: `http://localhost:${PORT}/assets/images/posts/dish_5.jpg`,
+        author: "60db8137d1903a2ea01edf54",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fermentum pharetra mauris vitae bibendum. Duis gravida tellus mauris, in fermentum massa pharetra non. Aliquam rhoncus faucibus mauris, a auctor nulla feugiat nec. Quisque sed pharetra lorem. Quisque egestas, odio a cursus cursus, lectus ipsum tristique odio, sit amet condimentum tortor. ",
       }).save(),
       new Post({
         image: `http://localhost:${PORT}/assets/images/posts/dish_6.jpg`,
+        author: "60db8137d1903a2ea01edf55",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fermentum pharetra mauris vitae bibendum. Duis gravida tellus mauris, in fermentum massa pharetra non. Aliquam rhoncus faucibus mauris, a auctor nulla feugiat nec. Quisque sed pharetra lorem. Quisque egestas, odio a cursus cursus, lectus ipsum tristique odio, sit amet condimentum tortor. ",
       }).save(),
       new Reward({
         price: 15,
