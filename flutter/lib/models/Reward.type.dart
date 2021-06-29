c89d8cf820c4f8f3644fd1ae758b2776e7e11d04
MaximeArn <maxime-arnould@outlook.fr>
2021-06-29 18:16:08 +0200

    code the named constructor to create Reward from a json

--- flutter/lib/models/Reward.type.dart
+++ flutter/lib/models/Reward.type.dart
@@ -1,14 +1,24 @@
 class Reward {
+  final String id;
   final int price;
   final String title;
   final String description;
-  final String logo;
+  // final String logo;
   final String image;
 
-  Reward(
-      {required this.price,
-      required this.title,
-      required this.description,
-      required this.logo,
-      required this.image});
+  Reward({
+    required this.id,
+    required this.price,
+    required this.title,
+    required this.description,
+    // required this.logo,
+    required this.image,
+  });
+
+  Reward.fromJson(Map<String, dynamic> json)
+      : id = json["id"],
+        price = json["price"],
+        title = json["title"],
+        description = json["description"],
+        image = json["image"];
 }
