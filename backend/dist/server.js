1d7e455ca9aae312a2b8391cb2de6bcfc6391104
MaximeArn <maxime-arnould@outlook.fr>
2021-06-29 11:31:31 +0200

    create express server

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const server = express_1.default();
+const port = 3000;
+server.get("/", (_, res) => {
+    res.send("Hello world !");
+});
+server.listen(port, () => console.log(`server is listening on ${port}`));
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,10 @@
-console.log("hello world");
+import express from "express";
+
+const server = express();
+const port = 3000;
+
+server.get("/", (_, res) => {
+  res.send("Hello world !");
+});
+
+server.listen(port, () => console.log(`server is listening on ${port}`));
