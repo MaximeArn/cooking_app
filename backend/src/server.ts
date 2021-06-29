f4e92ed38dcf3e530c0b103bf4f04c9f7012c03e
MaximeArn <maxime-arnould@outlook.fr>
2021-06-29 14:30:27 +0200

    set env varables for database and connect the db

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,29 @@
 import express from "express";
+import mongoose from "mongoose";
+import { config } from "dotenv";
 import userRouter from "./routers/user";
 
+config();
 const server = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PWD = process.env.DB_PWD;
+const DB_NAME = process.env.DB_NAME;
 
-server.use(express.static("../public"));
+mongoose.set("debug", true);
+mongoose.connect(
+  `mongodb+srv://${DB_USERNAME}:${DB_PWD}@cluster0.elh3w.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  }
+);
+
+server.use(express.static(`${__dirname}../public`));
 server.use("/user", userRouter);
 
 server.get("/", (_, res) => {
   res.send("Hello world !!!");
 });
 
-server.listen(port, () => console.log(`server is listening on ${port}`));
+server.listen(PORT, () => console.log(`server is listening on ${PORT}`));
