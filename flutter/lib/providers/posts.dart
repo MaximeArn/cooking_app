c1f56a0fbe4ea16d7192ee256fec6f10b278bbbb
MaximeArn <maxime-arnould@outlook.fr>
2021-06-29 21:18:55 +0200

    post provider to fetch posts

--- flutter/lib/providers/posts.dart
+++ flutter/lib/providers/posts.dart
@@ -1,1 +1,18 @@
+import 'dart:collection';
+import 'package:cooking/environment/env.dart';
+import 'package:http/http.dart' as http;
+import 'package:cooking/models/Post.type.dart';
+import 'package:flutter/material.dart';
 
+class PostsProvider with ChangeNotifier {
+  List<Post> _posts = [];
+
+  UnmodifiableListView<Post> get posts => UnmodifiableListView(_posts);
+
+  Future<void> fetchPosts() async {
+    http.Response response = await http.get(Uri.parse("$serverUrl/posts"));
+    if (response.statusCode == 200) {
+      notifyListeners();
+    }
+  }
+}
