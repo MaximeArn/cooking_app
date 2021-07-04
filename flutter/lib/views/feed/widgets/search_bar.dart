71c1650496b19481c5dea542791a64433c7696c2
MaximeArn <maxime-arnould@outlook.fr>
2021-07-04 15:39:31 +0200

    style the search bar

--- flutter/lib/views/feed/widgets/search_bar.dart
+++ flutter/lib/views/feed/widgets/search_bar.dart
@@ -5,15 +5,42 @@ class FeedSearchBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 5),
-      child: TextField(
-        decoration: InputDecoration(
-          hintText: "Search",
-          focusedBorder: UnderlineInputBorder(
-            borderSide: BorderSide(color: Colors.white),
-          ), 
-        ),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.white,
+                    size: 25,
+                  ),
+                  hintText: "Search",
+                  hintStyle: TextStyle(color: Colors.white),
+                  border: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                  enabledBorder: InputBorder.none,
+                  errorBorder: InputBorder.none,
+                  disabledBorder: InputBorder.none,
+                  ),
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.clear,
+              color: Colors.white,
+            ),
+          )
+        ],
       ),
-      color: Color.fromRGBO(232,196,81,.7,),
+      color: Color.fromRGBO(
+        232,
+        196,
+        81,
+        .7,
+      ),
     );
   }
 }
