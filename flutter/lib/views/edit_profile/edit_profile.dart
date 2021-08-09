cb723994eb57796ea60e61b57156cc4d00d83158
MaximeArn <maxime-arnould@outlook.fr>
2021-08-09 13:27:57 +0200

    avatar preview

--- flutter/lib/views/edit_profile/edit_profile.dart
+++ flutter/lib/views/edit_profile/edit_profile.dart
@@ -1,5 +1,8 @@
+import 'package:cooking/models/User.dart';
+import 'package:cooking/providers/users.dart';
 import 'package:cooking/widgets/scaffolds/secondary_scaffold.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class EditProfile extends StatefulWidget {
   static const routeName = "/editProfile";
@@ -11,6 +14,49 @@ class EditProfile extends StatefulWidget {
 class _EditProfileState extends State<EditProfile> {
   @override
   Widget build(BuildContext context) {
-    return SecondaryScaffold(body: Container());
+    User user = Provider.of<UsersProvider>(context).connectedUser as User;
+
+    return SecondaryScaffold(
+        body: Container(
+      margin: EdgeInsets.only(bottom: 80),
+      child: GestureDetector(
+        onTap: () {},
+        child: Container(
+          alignment: Alignment.topCenter,
+          child: Stack(
+            children: [
+              Container(
+                width: 130,
+                height: 130,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 1.5),
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(user.avatar),
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 5,
+                right: 5,
+                child: Container(
+                  height: 30,
+                  width: 30,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white,
+                      border: Border.all(color: Colors.black, width: 1.5)),
+                  child: Icon(
+                    Icons.edit,
+                    size: 19,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    ));
   }
 }
