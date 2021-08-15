d5a5219e63e0f7c5fe0daa9601725567e89c523d
MaximeArn <maxime-arnould@outlook.fr>
2021-08-15 21:01:26 +0200

    code function to compare password and hash

--- backend/src/utils/passwordHash.ts
+++ backend/src/utils/passwordHash.ts
@@ -1,3 +1,8 @@
 import bcrypt from "bcrypt";
 
 export const hashPassword = (clearPwd: string) => bcrypt.hash(clearPwd, 10);
+
+export const comparePassword = (hash: string, clearPwd: string) =>
+  bcrypt.compare(clearPwd, hash, (err, res) => {
+    err ? console.log(err) : console.log(res);
+  });
