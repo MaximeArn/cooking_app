59b7ccc60e8be49f8707b0ec4c31cc2fd50dd825
MaximeArn <maxime-arnould@outlook.fr>
2021-08-25 14:39:21 +0200

    comment an dremove logs

--- backend/src/controllers/images.ts
+++ backend/src/controllers/images.ts
@@ -8,6 +8,7 @@ module.exports = {
     { file: { filename, destination, path }, body: { oldAvatar } },
     res: Response
   ) => {
+    // remove previous avatar
     const previousAvatarPath = oldAvatar.replace("http://localhost:4545/", "");
     const absolutePreviousAvatarPath = Path.resolve(
       "public",
@@ -15,14 +16,15 @@ module.exports = {
     );
     fs.unlinkSync(absolutePreviousAvatarPath);
 
+    // compress new avatar
     const compressedFilename = `${filename.substring(
       0,
       filename.length - 4
     )}-compressed.jpg`;
     const compressedAvatarPath = Path.resolve(destination, compressedFilename);
-    console.log(compressedAvatarPath);
     await sharp(path).resize(180, 180).toFile(compressedAvatarPath);
 
+    // remove full size avatar
     fs.unlinkSync(Path.resolve(path));
 
     const newPath = `http://localhost:4545/assets/images/avatars/${compressedFilename}`;
