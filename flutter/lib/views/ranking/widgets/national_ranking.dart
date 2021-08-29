d324bb37e0aea3ab21b9060df066220d956b553c
MaximeArn <maxime-arnould@outlook.fr>
2021-08-29 22:29:00 +0200

    add ListTiles in teh listView

--- flutter/lib/views/ranking/widgets/national_ranking.dart
+++ flutter/lib/views/ranking/widgets/national_ranking.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class NationalRanking extends StatefulWidget {
-  const NationalRanking({ Key? key }) : super(key: key);
+  const NationalRanking({Key? key}) : super(key: key);
 
   @override
   _NationalRankingState createState() => _NationalRankingState();
@@ -11,8 +11,20 @@ class _NationalRankingState extends State<NationalRanking> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.all(25),
       alignment: Alignment.center,
-      child: Text("National !"),
+      child: ListView.separated(
+        itemBuilder: (_, __) => Card(
+            child: ListTile(
+          leading: Icon(Icons.star),
+          title: Text("User"),
+          trailing: Text(
+            35.toString(),
+          ),
+        )),
+        separatorBuilder: (_, __) => Divider(),
+        itemCount: 5,
+      ),
     );
   }
 }
