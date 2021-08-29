2f11872042dc8bb7b7d49d1277a53c17653596b7
MaximeArn <maxime-arnould@outlook.fr>
2021-08-29 22:37:52 +0200

    style and prepare the push of profile view

--- flutter/lib/views/ranking/widgets/national_ranking.dart
+++ flutter/lib/views/ranking/widgets/national_ranking.dart
@@ -1,3 +1,4 @@
+import 'package:cooking/views/profiles/profile/profile.dart';
 import 'package:flutter/material.dart';
 
 class NationalRanking extends StatefulWidget {
@@ -10,19 +11,29 @@ class NationalRanking extends StatefulWidget {
 class _NationalRankingState extends State<NationalRanking> {
   @override
   Widget build(BuildContext context) {
+    final double width = MediaQuery.of(context).size.width;
+
     return Container(
-      padding: EdgeInsets.all(25),
+      color: Colors.white,
+      padding: EdgeInsets.symmetric(vertical: 35, horizontal: 20),
       alignment: Alignment.center,
       child: ListView.separated(
         itemBuilder: (_, __) => Card(
             child: ListTile(
+              onTap: () => Navigator.pushNamed(context, Profile.routeName, arguments: "REMPLACER PAR L'ID"),
           leading: Icon(Icons.star),
           title: Text("User"),
           trailing: Text(
             35.toString(),
           ),
         )),
-        separatorBuilder: (_, __) => Divider(),
+        separatorBuilder: (_, __) => Divider(
+          indent: 0.30 * width,
+          endIndent: 0.30 * width,
+          height: 20,
+          thickness: 2,
+          color: Colors.black38,
+        ),
         itemCount: 5,
       ),
     );
