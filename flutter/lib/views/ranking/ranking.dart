8d53dd399ad300222da3b10fe23599b7e5dab0d2
MaximeArn <maxime-arnould@outlook.fr>
2021-08-29 19:19:48 +0200

    add tabBar in ranking view

--- flutter/lib/views/ranking/ranking.dart
+++ flutter/lib/views/ranking/ranking.dart
@@ -3,8 +3,21 @@ import 'package:flutter/material.dart';
 class Ranking extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: const Text("ranking"),
+    return DefaultTabController(
+      length: 3,
+      child: Container(
+        child: TabBar(tabs: [
+          Tab(
+            icon: Icon(Icons.cloud_outlined),
+          ),
+          Tab(
+            icon: Icon(Icons.beach_access_sharp),
+          ),
+          Tab(
+            icon: Icon(Icons.brightness_5_sharp),
+          ),
+        ]),
+      ),
     );
   }
 }
--- flutter/lib/widgets/scaffolds/cooking_app_bar.dart
+++ flutter/lib/widgets/scaffolds/cooking_app_bar.dart
@@ -8,28 +8,42 @@ class CookingAppBar extends StatelessWidget with PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      iconTheme: const IconThemeData(
-        size: 20,
-      ),
-      actions: [
-        if (isOwnProfile)
-          IconButton(
-            onPressed: () =>
-                Navigator.pushNamed(context, EditProfile.routeName),
-            icon: Icon(Icons.edit),
-          ),
-      ],
-      title: RichText(
-        text: const TextSpan(
-            style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-            children: [
-              TextSpan(text: "Coo", style: TextStyle(color: Colors.black)),
-              TextSpan(
-                  text: "K",
-                  style: TextStyle(color: Color.fromRGBO(232, 196, 81, 1))),
-              TextSpan(text: "ing", style: TextStyle(color: Colors.black)),
-            ]),
+    return DefaultTabController(
+      length: 3,
+      child: AppBar(
+        iconTheme: const IconThemeData(
+          size: 20,
+        ),
+        // bottom: const TabBar(tabs: [
+        //   Tab(
+        //     icon: Icon(Icons.cloud_outlined),
+        //   ),
+        //   Tab(
+        //     icon: Icon(Icons.beach_access_sharp),
+        //   ),
+        //   Tab(
+        //     icon: Icon(Icons.brightness_5_sharp),
+        //   ),
+        // ]),
+        actions: [
+          if (isOwnProfile)
+            IconButton(
+              onPressed: () =>
+                  Navigator.pushNamed(context, EditProfile.routeName),
+              icon: Icon(Icons.edit),
+            ),
+        ],
+        title: RichText(
+          text: const TextSpan(
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              children: [
+                TextSpan(text: "Coo", style: TextStyle(color: Colors.black)),
+                TextSpan(
+                    text: "K",
+                    style: TextStyle(color: Color.fromRGBO(232, 196, 81, 1))),
+                TextSpan(text: "ing", style: TextStyle(color: Colors.black)),
+              ]),
+        ),
       ),
     );
   }
