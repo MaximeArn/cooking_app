a902f3c530c61bce3063387b03ba9bde26628e64
MaximeArn <maxime-arnould@outlook.fr>
2021-10-12 21:47:46 +0200

    add a GridView on addMembers page to list selectedMembers

--- flutter/lib/views/ranking/widgets/groups_ranking/add_members.dart
+++ flutter/lib/views/ranking/widgets/groups_ranking/add_members.dart
@@ -1,6 +1,9 @@
+import 'package:cooking/environment/env.dart';
+import 'package:cooking/models/User.dart';
+import 'package:flutter/material.dart';
 import 'package:cooking/widgets/scaffolds/secondary_scaffold.dart';
 import 'package:cooking/widgets/searchBar/search_bar.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class AddMembers extends StatelessWidget {
   static const routeName = "/createGroup";
@@ -8,11 +11,119 @@ class AddMembers extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<User> selectedMembers = [
+      User(
+          id: "id",
+          age: 3,
+          name: "name",
+          email: "email",
+          password: "password",
+          avatar: "$serverUrl/assets/images/avatars/profile_photo_1.jpg",
+          posts: [],
+          stars: 3,
+          subscribers: [],
+          subscriptions: [],
+          countryCode: "countryCode",
+          groups: []),
+      User(
+          id: "id",
+          age: 3,
+          name: "name",
+          email: "email",
+          password: "password",
+          avatar: "$serverUrl/assets/images/avatars/profile_photo_2.jpg",
+          posts: [],
+          stars: 3,
+          subscribers: [],
+          subscriptions: [],
+          countryCode: "countryCode",
+          groups: []),
+      User(
+          id: "id",
+          age: 3,
+          name: "name",
+          email: "email",
+          password: "password",
+          avatar: "$serverUrl/assets/images/avatars/profile_photo_3.jpg",
+          posts: [],
+          stars: 3,
+          subscribers: [],
+          subscriptions: [],
+          countryCode: "countryCode",
+          groups: []),
+      User(
+          id: "id",
+          age: 3,
+          name: "name",
+          email: "email",
+          password: "password",
+          avatar: "$serverUrl/assets/images/avatars/profile_photo_4.jpg",
+          posts: [],
+          stars: 3,
+          subscribers: [],
+          subscriptions: [],
+          countryCode: "countryCode",
+          groups: []),
+      User(
+          id: "id",
+          age: 3,
+          name: "name",
+          email: "email",
+          password: "password",
+          avatar: "$serverUrl/assets/images/avatars/profile_photo_5.jpg",
+          posts: [],
+          stars: 3,
+          subscribers: [],
+          subscriptions: [],
+          countryCode: "countryCode",
+          groups: []),
+      User(
+          id: "id",
+          age: 3,
+          name: "name",
+          email: "email",
+          password: "password",
+          avatar: "$serverUrl/assets/images/avatars/profile_photo_1.jpg",
+          posts: [],
+          stars: 3,
+          subscribers: [],
+          subscriptions: [],
+          countryCode: "countryCode",
+          groups: []),
+    ];
+
     return SecondaryScaffold(
       body: Container(
         child: Column(
           children: [
-            FeedSearchBar(isFullPage: true, addMembersPage: true,)
+            if (selectedMembers.length > 0)
+              Expanded(
+                flex: 11,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  child: GridView.count(
+                    scrollDirection: Axis.horizontal,
+                    crossAxisCount: 1,
+                    mainAxisSpacing: 15,
+                    crossAxisSpacing: 15,
+                    children: selectedMembers
+                        .map((member) => GestureDetector(
+                          onTap: (){},
+                          child: CircleAvatar(
+                                backgroundImage: NetworkImage(member.avatar),
+                              ),
+                        ))
+                        .toList(),
+                  ),
+                ),
+              ),
+            Expanded(
+              flex: 89,
+              child: FeedSearchBar(
+                isFullPage: true,
+                addMembersPage: true,
+              ),
+            )
           ],
         ),
       ),
