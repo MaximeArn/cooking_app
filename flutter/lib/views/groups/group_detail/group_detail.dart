da2d7b1c80b26f8721a932b7530ec43ff58f63e3
MaximeArn <maxime-arnould@outlook.fr>
2021-10-26 18:00:44 +0200

    add the tabBar of the groupDetail

--- flutter/lib/views/groups/group_detail/group_detail.dart
+++ flutter/lib/views/groups/group_detail/group_detail.dart
@@ -11,9 +11,39 @@ class GroupDetail extends StatelessWidget {
     final Group group = ModalRoute.of(context)!.settings.arguments as Group;
 
     return SecondaryScaffold(
-      body: Container(
-        alignment: Alignment.center,
-        child: Text(group.name as String),
+      body: DefaultTabController(
+        length: 2,
+        child: Container(
+          child: Column(
+            children: [
+              TabBar(
+                unselectedLabelColor: Colors.black54,
+                labelColor: Theme.of(context).highlightColor,
+                indicatorColor: Theme.of(context).highlightColor,
+                indicatorWeight: 2.5,
+                tabs: [
+                  Tab(
+                    child: Text("Challenges"),
+                  ),
+                  Tab(
+                    child: Text("Ranking"),
+                  ),
+                ],
+              ),
+              Expanded(
+                flex:1,
+                child: TabBarView(children: [
+                  Center(
+                    child: Text("Challenges"),
+                  ),
+                  Center(
+                    child: Text("Ranking"),
+                  ),
+                ]),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
