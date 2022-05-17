9b171d5ced49c32e6bd7473004f95e7fa5747d5e
MaximeArn <maxime-arnould@outlook.fr>
2022-05-17 16:03:51 +0200

    add aws credentials to the env file

--- backend/dist/routers/user.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const userRouter = express_1.Router();
-userRouter.post("/", (req, res, next) => {
-    res.send("add user !");
-});
-exports.default = userRouter;
--- backend/src/routers/groups.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var _a = require("../controllers/groups"), createGroup = _a.createGroup, getGroups = _a.getGroups, getGroupById = _a.getGroupById;
-var groupsRouter = (0, express_1.Router)();
-groupsRouter.post("/:connectedUser", createGroup);
-groupsRouter.get("/getGroups/:connectedUser", getGroups);
-groupsRouter.get("/:groupId", getGroupById);
-exports["default"] = groupsRouter;
