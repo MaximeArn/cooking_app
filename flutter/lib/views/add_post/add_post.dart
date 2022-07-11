a33174d3c8635a58843c13ddda121e36199cf16b
MaximeArn <maxime-arnould@outlook.fr>
2022-07-11 16:42:38 +0200

    change UI and gesture dectectors

--- flutter/lib/views/add_post/add_post.dart
+++ flutter/lib/views/add_post/add_post.dart
@@ -1,4 +1,5 @@
 import 'package:camera/camera.dart';
+import 'package:cooking/widgets/loader.dart';
 import 'package:flutter/material.dart';
 import "../../main.dart";
 
@@ -14,11 +15,12 @@ class AddPost extends StatefulWidget {
 class _State extends State<AddPost> with WidgetsBindingObserver {
   CameraController? controller;
   bool _isCameraInitialized = false;
+  bool _isRearCameraSelected = true;
 
   @override
   void initState() {
     super.initState();
-    onNewCameraSelected(cameras[0]);
+    onNewCameraSelected(cameras[1]);
   }
 
   @override
@@ -60,9 +62,7 @@ class _State extends State<AddPost> with WidgetsBindingObserver {
       }
 
       cameraController.addListener(() {
-        if (mounted)
-          setState(() {
-          });
+        if (mounted) setState(() {});
       });
 
       try {
@@ -70,7 +70,6 @@ class _State extends State<AddPost> with WidgetsBindingObserver {
       } on CameraException catch (e) {
         print('Error initializing camera: $e');
       }
-
 
       if (mounted) {
         setState(() {
@@ -87,12 +86,63 @@ class _State extends State<AddPost> with WidgetsBindingObserver {
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.center,
-      child: _isCameraInitialized
-          ? AspectRatio(
-              aspectRatio: 1 / controller!.value.aspectRatio,
-              child: controller!.buildPreview(),
-            )
-          : Container(),
+      child: Stack(
+        children: [
+          _isCameraInitialized
+              ? Center(
+                  child: AspectRatio(
+                    aspectRatio: 1 / controller!.value.aspectRatio,
+                    child: controller!.buildPreview(),
+                  ),
+                )
+              : Container(
+                  child: Loader(),
+                ),
+          Container(
+            padding: EdgeInsets.only(bottom: 20, left: 20),
+            alignment: Alignment.bottomLeft,
+            child: Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _isCameraInitialized = false;
+                    });
+                    onNewCameraSelected(
+                      cameras[_isRearCameraSelected ? 0 : 1],
+                    );
+                    setState(() {
+                      _isRearCameraSelected = !_isRearCameraSelected;
+                    });
+                  },
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Icon(
+                        Icons.circle,
+                        color: Colors.black38,
+                        size: 60,
+                      ),
+                      Icon(
+                        _isRearCameraSelected
+                            ? Icons.camera_front
+                            : Icons.camera_rear,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                    ],
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    print("take picture !");
+                  },
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
