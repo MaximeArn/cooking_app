647e65bfdbf6a61032f5b16e95ec2f77c40eca57
MaximeArn <maxime-arnould@outlook.fr>
2022-11-18 16:04:51 +0100

    remove dead code

--- backend/src/routers/rewards.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-const { getRewards } = require("../controllers/rewards");
-
-const rewardsRouter = Router();
-
-rewardsRouter.get("/", getRewards);
-
-export default rewardsRouter;
